d8406c999a11f8d341ef76bf559796f5de896a97
dkagotho <dianakago2@gmail.com>
2020-02-23 14:24:12 -0500

    appended and added value to the container

--- homework5/Develop/script.js
+++ homework5/Develop/script.js
@@ -5,13 +5,46 @@ $("#currentDay").text(moment().format("dddd MMMM Do YYYY"));
 
 });
 
+// var hours = [];
+// for( var i=9; i<12; i++ ) {
+//     hours.push(i+"AM");
+// }
+// hours.push("12PM")
+// for(var i=1; i<6; i++) {
+//     hours.push(i+ "PM");
+// }
+// console.log(hours);
 
-var hours = [];
-for( var i=9; i<12; i++ ) {
-    hours.push(i+"AM");
+var date = new Date;
+var hour = date.getHours();
+
+var pastHours = [],
+    futureHours = [];
+for( var i=9; i<hour; i++ ) {
+    if (i>=13){
+        pastHours.push(i-12 + ":00");   
+    }
+    else{
+        pastHours.push(i+":00");
+    }
 }
-hours.push("12PM")
-for(var i=1; i<6; i++) {
-    hours.push(i+ "PM");
+for(var i=hour; i<18; i++) {
+  if (i>=13){
+    futureHours.push(i-12 + ":00");   
+    }
+    else{
+   futureHours.push(i+":00");
+    }
 }
-console.log(hours);
+
+console.log(pastHours);
+console.log(futureHours);
+
+for (let index = 0; index < pastHours.length; index++) {
+    var element = pastHours[index];
+    $(".container").append("<div class= 'row hour past'>"+element+"</div>");
+}
+for (let index = 0; index < futureHours.length; index++) {
+    var element = futureHours[index];
+    $(".container").append("<div class= 'row hour future'>"+element+"</div>");
+}
