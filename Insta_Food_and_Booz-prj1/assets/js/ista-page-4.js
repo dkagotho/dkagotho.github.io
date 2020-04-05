6d7346b7ee4211e2590eba14ee0d55b9d886cf19
dkagotho <dianakago2@gmail.com>
2020-04-05 16:26:47 -0400

    added group project

---
+++ Insta_Food_and_Booz-prj1/assets/js/ista-page-4.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+
+    
+    var urlParams = new URLSearchParams(window.location.search);
+
+  
+
+    var mealID = urlParams.get('mealID');
+    var drinkID =urlParams.get('drinkID');
+
+    mealUrl ="https://www.themealdb.com/api/json/v1/1/lookup.php?i="+mealID+"&api-key=1"
+    drinkUrl = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="+drinkID+"&api-key=1"
+
+    $.ajax({
+        url: mealUrl,
+        methog: "GET"
+
+    }).then(function(response){
+
+        console.log(response.meals[0])
+
+        $("#foodImage").append("<li class=caralho >"+response.meals[0].strMeal+"</li>")
+        $("#foodImage").append("<li><img src="+response.meals[0].strMealThumb+" alt="+response.meals[0].strMeal +"></li>");
+        console.log(response.meals[0].strIngredient1)
+        
+
+        for (let index = 1; index < 20; index++) {
+
+                var ingredient = "strIngredient"+index;
+                var measure = "strMeasure"+index;
+
+                if(response.meals[0][ingredient]){
+                    // $("#igredientsFood").append("<li>"+res^ponse.meals[0][ingredient]+"</li>") 
+                    $("#igredientsFood").append( '<tr><td>' + response.meals[0][ingredient] +"</td><td>" + response.meals[0][measure]+ '</td></tr>' );
+                }
+
+           
+            console.log(response.meals[0][ingredient])
+            
+        }
+       
+
+
+
+    })
+
+    $.ajax({
+        url: drinkUrl,
+        methog: "GET"
+
+    }).then(function(response){
+
+        console.log(response.drinks[0])
+
+        $("#drinkImage").append("<li class= caralho>"+response.drinks[0].strDrink+"</li>")
+        $("#drinkImage").append("<li><img src="+response.drinks[0].strDrinkThumb+" alt="+response.drinks[0].strDrink +"></li>");
+
+        for (let index = 1; index < 20; index++) {
+
+            var ingredient = "strIngredient"+index;
+            var measure = "strMeasure"+index;
+
+
+            if(response.drinks[0][ingredient]){
+                // $("#igredientsDrink").append("<li>"+response.drinks[0][ingredient]+"</li>") 
+                $("#igredientsDrink").append( '<tr><td>' + response.drinks[0][ingredient] +"</td><td>" + response.drinks[0][measure]+ '</td></tr>' );
+            }
+
+       
+        console.log(response.drinks[0][ingredient])
+        
+    }
+        
+        
+
+
+    })
+    
+
+
+})
---
+++ Insta_Food_and_Booz-prj1/assets/js/script.js
@@ -0,0 +1,184 @@
+console.log("hello")
+$(document).ready(function(){
+
+  
+  var getTypes = "https://www.themealdb.com/api/json/v1/1/list.php?c=list&api-key=1"
+  var getAreas = "https://www.themealdb.com/api/json/v1/1/list.php?a=list&api-key=1"
+
+
+  $.ajax({
+
+    url: getTypes,
+    method: "GET"
+    
+  }).then(function(response){
+
+    console.log(response.meals)
+
+    for (let index = 0; index < response.meals.length; index++) {
+      $("#foodRestriction").append("<option>"+ response.meals[index].strCategory + "</option>" )
+      
+    }
+
+
+  })
+
+
+
+  $.ajax({
+
+    url: getAreas,
+    method: "GET"
+    
+  }).then(function(response){
+
+    console.log(response.meals)
+
+    for (let index = 0; index < response.meals.length; index++) {
+      $("#search-term").append("<option>"+ response.meals[index].strArea + "</option>" )
+      
+    }
+
+
+  })
+
+
+
+
+    $("#mealTypeSelection").on("click", function(event){
+
+        var mealTypeSelection = event.target.id
+
+        console.log(mealTypeSelection);
+        window.location.href = "./insta-choice.html?mealtype=" + mealTypeSelection;
+    })
+    $('#answer').on('click', 'p', function(event){
+        console.log("did this really work??? ", event.target.id)
+
+        var mealIDforDisplay = event.target.id;
+
+        var alcohoSuggestionURL = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list&api-key=1"
+
+        $.ajax({
+          url: alcohoSuggestionURL,
+          method: "GET"
+
+        }).then(function(response){
+
+            console.log(response.drinks);
+
+            
+
+           var alcoholSuggestion = Math.floor(Math.random() * response.drinks.length)
+
+          
+
+            console.log("how a about a glass of "+response.drinks[alcoholSuggestion].strIngredient1+" with that?")
+            
+            var cocktailURL ="https://www.thecocktaildb.com/api/json/v1/1/filter.php?i="+response.drinks[alcoholSuggestion].strIngredient1+"&api-key=1"
+
+            $.ajax({
+                url: cocktailURL,
+                method: "GET"
+
+            }).then(function(response){
+
+              console.log(response)
+              var cocktailSuggestion = Math.floor(Math.random() * response.drinks.length)
+
+              console.log("how does "+ response.drinks[cocktailSuggestion].strDrink+" sound?")
+
+              var drinkIDforDisplay = response.drinks[cocktailSuggestion].idDrink;
+
+            //  localStorage.setItem(response.drinks[coc], eventToSave);
+
+           window.location.href = "./ista-page-4.html?mealID="+mealIDforDisplay+"&drinkID="+drinkIDforDisplay;
+
+
+           
+
+
+            })
+
+
+        })
+
+      
+
+
+    });
+        
+
+
+
+
+
+  $("#btnSearch").on("click", function() {
+      event.preventDefault();
+      $("#answer").empty();
+
+      var typesMealIDS =[]
+      var typeTerm = $("#foodRestriction").val();
+      var searchTerm = $("#search-term").val();
+
+      var getTypesMeals = "https://www.themealdb.com/api/json/v1/1/filter.php?c=" +
+      typeTerm +
+      "&api-key=1";
+    
+    
+      var getURL =
+        "https://www.themealdb.com/api/json/v1/1/filter.php?a=" +
+        searchTerm +
+        "&api-key=1";
+
+        $.ajax({
+          url: getTypesMeals,
+          method: "GET"
+        }).then(function(response) {
+          //console.log(response.meals.length)
+      
+          console.log(response)
+      
+          for (let index = 0; index < response.meals.length; index++) {
+            
+            typesMealIDS.push(response.meals[index].idMeal)
+          
+          }
+
+          console.log(typesMealIDS)
+        });
+      
+      $.ajax({
+        url: getURL,
+        method: "GET"
+      }).then(function(response) {
+        //console.log(response.meals.length)
+    
+        console.log(response)
+    
+        for (let index = 0; index < response.meals.length; index++) {
+        
+
+
+
+          if (typesMealIDS.includes(response.meals[index].idMeal)) {  
+            console.log(response.meals[index].idMeal)
+            $("#answer").append(`<div class=menu-choice id=menu${index}></div>`)
+            $(`#menu${index}`).append("<p class=foodLink id="+response.meals[index].idMeal+">"+response.meals[index].strMeal +"</p>");
+            // $("#answer").append("<p class=foodLink id="+response.meals[index].idMeal+">"+response.meals[index].strMeal +"</p>");
+            //$("#answer").append("<p class=foodLink id="+response.meals[index].idMeal+"><a href=./index.html>"+response.meals[index].strMeal +"</a></p>");
+            $(`#menu${index}`).append("<p><img src="+response.meals[index].strMealThumb+" alt="+response.meals[index].strMeal +"></p>");
+            
+
+          }else {"the id it not there " + response.meals[index].idMeal }
+
+        
+
+
+        }
+      });
+    });
+});  
+
+
+  
