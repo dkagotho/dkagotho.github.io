0d1e6e89d587b54e156532ff1681dea2d1d55596
dkagotho <dianakago2@gmail.com>
2020-04-20 22:53:31 -0400

    added init function

--- homework9/Develop/index.js
+++ homework9/Develop/index.js
@@ -1,12 +1,44 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const axios = require("axios");
 
+
+let questions = [
+    {
+    type: "input",
+    message: "What is your GitHub user name?",
+    name: "username"
+      }
 ];
 
+
 function writeToFile(fileName, data) {
+    
 }
 
 function init() {
-
+    inquirer
+    .prompt(questions).then(function(response) 
+    {const queryUrl = `https://api.github.com/users/${username}`;
+    axios.get(queryUrl).then(function(res) {
+      console.log("Response.Data = ", res.data);
+      const repoNames = res.data.map(function(repo) {
+        return repo.name;
+      });
+      const repoNamesStr = repoNames.join("\n");
+      questions.push(
+        {
+          type: "list",
+          message: "Choose your repository: ",
+          name: "repository",
+          choices: repoNamesStr
+        }
+      );
+    console.log(response.username); 
+    });
 }
 
 init();
+
+
+ 
