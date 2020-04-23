eb81b749e0631776e45bcae63626b070b17ef97c
dkagotho <dianakago2@gmail.com>
2020-04-22 21:26:08 -0400

    added init function

--- homework9/Develop/index.js
+++ homework9/Develop/index.js
@@ -55,32 +55,73 @@ let sections = [
   }
 ];
 
-
 function writeToFile(fileName, data) {
-    
+  for (let index = 0; index < fileName.length; i++) {
+    console.log(`Creating "${fileName[i]}" file`)
+
+    fs.writeFileSync(path.join(process.cwd(), fileName[i]), data[i], function(err){
+      if (err) {
+        console.log(err);
+      }
+    });
+    console.log(`"${fileName[i]}" file created!`); 
+  } 
 }
 
 function init() {
-    inquirer
-    .prompt(questions).then(function(response) 
-    {const queryUrl = `https://api.github.com/users/${username}`;
-      axios.get(queryUrl).then(function(res) {
-        console.log("Response.Data = ", res.data);
-        const repoNames = res.data.map(function(repo) {
-          return repo.name;
-        });
-        const repoNamesStr = repoNames.join("\n");
-        questions.push(
-          {
-            type: "list",
-            message: "Choose your repository: ",
-            name: "repository",
-            choices: "repoNamesStr"
-          }
-        );
-        console.log(response.username); 
-        });
+  inquirer.prompt(questions[0]).then( (response_Q0) => {
+    api.getUser(response_Q0.username).then((resUser) => {
+      // console.log("Response.Data = ", resUser.data);
+
+      api.getRepository(response_Q0.username).then((resRepo) => {
+        const repoNames = resRepo.data.map((repository) => repository.name);
+        // console.log("Chosen One: ", resRepo.data);
+
+        if (repoNames.length > 0) {
+          // Put ALL repositories as options to the user
+          questions.push( 
+            {
+              type:    "list",
+              name:    "repolist",
+              message: 'Please, choose 1 (one) repository to create your new "Readme.md" file:',
+              choices: repoNames
+            }
+          );
+
+          inquirer.prompt(questions[questions.length - 1]).then( (response_Q1) => {
+            // console.log(`Data from Chosen One: ${resRepo.data[questions[questions.length - 1].choices.indexOf(response_Q1.repolist)].name}`);
+            inquirer.prompt(requirements).then( (responseRequirements) => {
+              writeToFile(generateMarkdown(resUser.data, resRepo.data[questions[questions.length - 1].choices.indexOf(response_Q1.repolist)], responseRequirements, repoNames), "README.md", `${resUser.data.login}.pdf`);
+            });
+          });
+        }
+        else
+          console.log(`WARNING: "${response_Q0.username}" does not have a repository.`);
+      });
     });
+  });
+}
+
+    // inquirer
+    // .prompt(question).then(function(response) 
+    // {const queryUrl = `https://api.github.com/users/${username}`;
+    //   axios.get(queryUrl).then(function(res) {
+    //     console.log("Response.Data = ", res.data);
+    //     const repoNames = res.data.map(function(repo) {
+    //       return repo.name;
+    //     });
+    //     const repoNamesStr = repoNames.join("\n");
+    //     questions.push(
+    //       {
+    //         type: "list",
+    //         message: "Choose your repository: ",
+    //         name: "repository",
+    //         choices: "repoNamesStr"
+    //       }
+    //     );
+    //     console.log(response.username); 
+    //     });
+    // });
 }
 
 init();
--- homework9/Develop/utils/api.js
+++ homework9/Develop/utils/api.js
@@ -1,6 +1,28 @@
+const axios = require("axios");
+const queryUrl     = "https://api.github.com/users/";
+const queryUrlRepo = "/repos?per_page=100";
+
 const api = {
   getUser(username) {
+    const queryUrlUsername = queryUrl.concat(username); 
 
+    return axios
+      .get(queryUrlUsername)
+      .catch(err => {
+        console.log("ERROR: Username not found.");
+        process.exit(1); 
+      });
+  },
+
+  getRepository(username) {
+    const queryUrlUsername = queryUrl.concat(username.concat(queryUrlRepo)); 
+
+    return axios
+      .get(queryUrlUsername)
+      .catch(err => {
+        console.log("ERROR: Repository URL not found.");
+        process.exit(1); 
+      });
   }
 };
 
